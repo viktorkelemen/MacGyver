5ca7965f97365c4fdc1e51e0c1ec8d2a4ed583fe
viktor <viktor@colopl.co.jp>
2011-02-03 17:48:49 +0900

    Optimizing the telltime.sh with a for in

--- telltime.sh
+++ telltime.sh
@@ -1,30 +1,18 @@
+timezones=( "Asia/Tokyo" "America/Los_Angeles" "America/New_York" "America/Chicago" "Europe/London" "")
+
 if [ "$*" ]
 then
-	TZ=Asia/Tokyo date -v$1
-	echo ""
-	TZ=America/Los_Angeles date -v$1
-	echo ""
-	TZ=America/New_York date -v$1
-	echo ""
-	TZ=America/Chicago date -v$1
-	echo ""
-	TZ=Europe/London date -v$1
-	echo ""
-	TZ=Europe/Budapest date -v$1
-	echo ""
+	for name in ${timezones[@]}
+	do
+	  echo TZ=$name date -v$1
+	  echo ""
+	done
 else
 	echo "Telling the current time in different timezones"
 	echo ""
-	TZ=Asia/Tokyo date
-	echo ""
-	TZ=America/Los_Angeles date
-	echo ""
-	TZ=America/New_York date
-	echo ""
-	TZ=America/Chicago date
-	echo ""
-	TZ=Europe/London date
-	echo ""
-	TZ=Europe/Budapest date
-	echo ""
+	for name in ${timezones[@]}
+	do
+	  echo TZ=$name date
+	  echo ""
+	done
 fi
