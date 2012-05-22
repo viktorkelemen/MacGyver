27f516454f575c76cf2ad135ccdf00af3ea71b9b
Viktor Kelemen <viktor@cookpad.jp>
2012-05-22 12:38:45 +0900

    Adding the remote debug port

--- chrome_start.sh
+++ chrome_start.sh
@@ -34,6 +34,7 @@ office)
 
 android)
   exec "$GOOGLE_CHROME" \
+    --remote-debugging-port=9222 \
     --enable-udd-profiles \
     --user-agent="Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1" \
     --user-data-dir="/Users/$USER/Library/Application Support/Google/Chrome/android_mobile"
